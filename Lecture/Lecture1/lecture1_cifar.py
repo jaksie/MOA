d3dba5e1328ba5ce1026ad18619840e7245aee3e
Jakub Siedlecki <3siedlecki@taurus.fis.agh.edu.pl>
2026-03-14 03:09:24 +0100

    finishied lec1

--- Lecture/Lecture1/lecture1_cifar.py
+++ Lecture/Lecture1/lecture1_cifar.py
@@ -1,19 +1,20 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-os.environ['TF_CPP_MIN_VLOG_LEVEL'] = '2'
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # supress warnings about CPU, namely that it is not using AVX2; 'CPP' stands for C++ (because tensorflow is written in C++)
+os.environ['TF_CPP_MIN_VLOG_LEVEL'] = '2' # supress verbose logging
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0' # supress warnings about oneDNN (Intel's Deep Neural Network library)
 
-import numpy as np
-import matplotlib.pyplot as plt
-from tensorflow.keras.datasets import cifar10
+import numpy as np 
+import matplotlib.pyplot as plt 
+from tensorflow.keras.datasets import cifar10 
 
 # time it
-import time
-start = time.time()
-
+start_time = time.time()
 # Load data
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-y_train = y_train.flatten()
+(X_train, y_train), (X_test, y_test) = cifar10.load_data() # 'X' is capitalized because it is a matrix, 'y' is lowercase because it is a vector
+end_time = time.time()
+print(f"Time taken to load data: {end_time - start_time}")
+
+y_train = y_train.flatten() # 
 
 end = time.time()
 print(f"Time taken to load data: {end - start}")
